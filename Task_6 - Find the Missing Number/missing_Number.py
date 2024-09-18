ed724b3642b737cf718d5933ce1cf18d3b5621d2
Gaganjeet <gaganjeetwork99@gmail.com>
2024-09-18 18:51:12 +0530

    Find the Missing Number

---
+++ Task_6 - Find the Missing Number/missing_Number.py
@@ -0,0 +1,19 @@
+## i have Using Cyclic sort 
+def find_missing(arr, n):
+    i = 0 
+    while i < n-1:
+        correct_index = arr[i] - 1
+        if arr[i] > 0 and arr[i] < n and arr[i] != arr[correct_index]:
+            arr[i], arr[correct_index] = arr[correct_index], arr[i]
+        else:
+            i+=1
+    for i in range(n - 1):
+        if  arr[i] != i+1:
+            return i+1
+    return n
+
+arr = [1, 2, 3, 4]
+n = 5
+print(find_missing(arr, n))  
+            
+            
