7f79e094ccb3c4fc867ae6ea1821e7a4827dcfd1
Gaganjeet <gaganjeetwork99@gmail.com>
2024-09-18 15:40:51 +0530

    updated the Task_4 value error message

--- Task_3 - Factorial Calculation/Factorial_Calculation.py
+++ Task_3 - Factorial Calculation/Factorial_Calculation.py
@@ -1,6 +1,6 @@
 def factorial(num):
     if num < 0:
-        return ValueError("Factorial is not defined for negative numbers.")
+        return ValueError("Factorial is not defined for negative numbers. Please provide a non-negative integer.")
     result = 1
     for i in range(1, num+1):
         result *= i
