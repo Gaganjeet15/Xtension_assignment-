822534e0ea5012802fb3a7b18a6f4b3d8c0db466
Gaganjeet <gaganjeetwork99@gmail.com>
2024-09-18 15:58:24 +0530

    Anagram Checker

---
+++ Task_5 - Anagram Checker/anagram_Checker.py
@@ -0,0 +1,11 @@
+def is_anagram(str1, str2):
+    
+    str1 = str1.replace(" ", "").lower()
+    str2 = str2.replace(" ", "").lower()
+    
+    return sorted(str1) == sorted(str2)
+
+
+print(is_anagram("hello", "world"))
+print(is_anagram("aabcc", "bccaa"))
+print(is_anagram("Xtension", "noisnetX"))
